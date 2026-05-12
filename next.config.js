120192fefc3c4291dd2594c45bec015f62134bc4
rjb4200 <75321245+rjb4200@users.noreply.github.com>
2026-05-12 15:16:41 -0400

    Trace PDFKit font assets for daily email route

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingIncludes: {
-    "/*": ["./node_modules/pdfkit/js/data/**/*"],
+    "/*": [
+      "./node_modules/pdfkit/js/data/**/*",
+    ],
+    "/api/cron/daily-email-report": [
+      "./node_modules/pdfkit/js/data/**/*",
+    ],
+    "/src/app/api/cron/daily-email-report/route": [
+      "./node_modules/pdfkit/js/data/**/*",
+    ],
   },
 }
 
